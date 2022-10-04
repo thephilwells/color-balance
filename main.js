05ce1591117387d902a94df849d622b26366d5dc
Phil Wells <phil.wells@epicgames.com>
2022-10-04 09:41:18 -0400

    more lil dudes

--- main.js
+++ main.js
@@ -474,7 +474,7 @@ function drawCursors() {
         players[i].y += players[i].vy;
       }
       uctx.drawImage(cursorImg, players[i].x, players[i].y, 25, 25);
-      sprite.drawSprite(uctx, players[0])
+      sprite.drawSprite(uctx, players[i])
     }
   }
   requestAnimationFrame(drawCursors);
--- sprite.js
+++ sprite.js
@@ -3,9 +3,36 @@ const spriteHeight = 16;
 const borderWidth = 1;
 const spacingWidth = 0;
 
-export function spritePositionToImagePosition(row, col) {
-  return {
-    x: borderWidth + col * (spacingWidth + spriteWidth),
-    y: borderWidth + row * (spacingWidth + spriteHeight),
-  };
+export class Sprite {
+  constructor(spriteImage) {
+    this.spriteImage = spriteImage;
+  }
+
+  spritePositionToImagePosition(row, col) {
+    return {
+      x: borderWidth + col * (spacingWidth + spriteWidth),
+      y: borderWidth + row * (spacingWidth + spriteHeight),
+    };
+  }
+
+  drawSprite(canvasContext, player) {
+    let [row, col] = this.getSpritePosition(player)
+    const spritePosition = this.spritePositionToImagePosition(row, col)
+    canvasContext.drawImage(
+      this.spriteImage,
+      spritePosition.x,
+      spritePosition.y,
+      16,
+      16,
+      player.x + 16,
+      player.y + 16,
+      16,
+      16
+    );
+  }
+
+  getSpritePosition(player) {
+    // TODO - make it real
+    return [1, 0];
+  }
 }
